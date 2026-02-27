015adcd3cd394c5fe83f2a9ba1d4c39e579cfd7f
mayank-devlogs <mayanksk.711@gmail.com>
2026-02-27 16:17:30 +0530

    UC7: Deque based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,31 +1,32 @@
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        System.out.println("Palindrome Checker App (UC7 - Deque)");
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
+        Deque<Character> deque = new LinkedList<>();
 
-        // Push into stack and enqueue into queue
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            stack.push(ch);
-            queue.add(ch);
+        // Add characters to deque
+        for (char ch : input.toCharArray()) {
+            if (Character.isLetterOrDigit(ch)) {
+                deque.add(Character.toLowerCase(ch));
+            }
         }
 
         boolean isPalindrome = true;
 
-        // Compare dequeue and pop
-        for (int i = 0; i < input.length(); i++) {
-            if (stack.pop() != queue.remove()) {
+        // Compare front and rear
+        while (deque.size() > 1) {
+            if (deque.pollFirst() != deque.pollLast()) {
                 isPalindrome = false;
                 break;
             }
